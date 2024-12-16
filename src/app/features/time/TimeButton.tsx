266c4a2f5663279803572d6a71460ece9ccf0928
DOONG2 <xklreader@gmail.com>
2024-12-16 17:41:07 +0900

    ADD: time diff 동기화

--- src/app/features/time/TimeButton.tsx
+++ src/app/features/time/TimeButton.tsx
@@ -13,14 +13,15 @@ export default function TimeButton({ className }: TimeButtonProps) {
   const [targetTime, setTargetTime] = useState<number | null>(
     Number(localStorage.getItem(TARGET_TIME))
   );
-  const [timeDiff, setTimeDiff] = useState(Number(targetTime) - currentTime);
+  const [timeDiff, setTimeDiff] = useState<number | null>(
+    targetTime ? targetTime - currentTime : null
+  );
   const [toggle, setToggle] = useState(false);
 
   const { data, isError, isLoading, isFetching, isSuccess } = useGetTimeQuery({
     toggle,
   });
 
-  console.log(currentTime, targetTime, timeDiff);
   const buttonText = isLoading || isFetching ? "loading.." : "Start";
   const isExistTargetTime = localStorage.getItem(TARGET_TIME) != null;
 
@@ -29,19 +30,21 @@ export default function TimeButton({ className }: TimeButtonProps) {
   useEffect(() => {
     if (!isExistTargetTime) return;
     const intervalId = setInterval(() => {
-      setCurrentTime(new Date().getTime);
-      console.log(currentTime);
-    }, 1000); // 1초마다 갱신
+      const currentTime = new Date().getTime();
+      setCurrentTime(currentTime);
+      if (targetTime != null) setTimeDiff(targetTime - currentTime);
+    }, 1000);
 
-    return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 인터벌 해제
-  }, [currentTime, isExistTargetTime, targetTime]);
+    console.log(currentTime, targetTime, timeDiff);
+
+    return () => clearInterval(intervalId);
+  }, [currentTime, isExistTargetTime, targetTime, timeDiff]);
 
   useEffect(() => {}, [targetTime]);
 
-  // TODO: if(isSuccess) 현재 시간 startTime 초기화 / targetTime 없으면 startTime + duration = targetTime 초기화 / localStorage에  targetTime - startTime 차이 저장
+  // TODO: diff 음수일때 로컬스토리지 삭제, 상태 init, Done 변경
   // TODO: 실시간 차이값 / 처음 차이값 계산해서 로딩레이어 넓이값 조절
   // TODO: 실시간 차이값 1초단위 반올림으로 로딩텍스트값에 싱크
-  // TODO: 실시간 차이값 < 0 아래일때 로컬스토리지 전부 삭제후 스토리지 유무로 텍스트 Done 변경
   // TODO: Done 텍스트 보이게 1초 지연후 Start 버튼 렌더
   // TODO: 전체 인터벌 1초 / 로딩 ui 남은시간 따로 인터벌
   // TODO: 인터벌 매끄럽지 못하면 애니메이션or더짧게
